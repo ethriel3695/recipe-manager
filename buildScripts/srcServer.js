74fd46b149b45026c744216a26e47c8e3f9b841f
Reuben Ellis <ethriel3695@gmail.com>
2018-03-11 11:35:39 -0600

    added production server

---
+++ buildScripts/srcServer.js
@@ -0,0 +1,40 @@
+import express from 'express';
+import path from 'path';
+import open from 'open';
+import webpack from 'webpack';
+import config from '../webpack.config.babel';
+import bodyParser from 'body-parser';
+
+/* eslint-disable no-console */
+const port = process.env.PORT || 3030;
+const app = express();
+
+const compiler = webpack(config);
+
+app.use(require('webpack-dev-middleware')(compiler, {
+  noInfo: true,
+  publicPath: config.output.publicPath
+}));
+
+app.use(require('webpack-hot-middleware')(compiler));
+
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+
+if (process.env.NODE_ENV === 'production') {
+  app.use(express.static('client/build'));
+
+  app.get('/', function (req, res) {
+    res.sendFile(path.join(__dirname, '../client/src/index.html'));
+  });
+}
+
+app.listen(port, function(err) {
+  if (err) {
+    console.log(err);
+  } else {
+    open(`http://localhost:${port}`);
+  }
+});
+
+export default app;
