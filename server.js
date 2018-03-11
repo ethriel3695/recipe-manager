dd1016d8c3652903be39c8a2183f1ac06e72929b
Reuben Ellis <ethriel3695@gmail.com>
2018-03-11 09:47:56 -0600

    fixed server.js

--- server.js
+++ server.js
@@ -7,7 +7,7 @@ const path = require('path');
 const port = process.env.PORT || 3000;
 const app = express();
 
-app.use(express.static('dist'));
+app.use(express.static('build'));
 
 app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'src/index.html'));
@@ -20,3 +20,19 @@ app.listen(port, function(err) {
     open('http://localhost:' + port);
   }
 });
+
+// if (process.env.NODE_ENV === 'production') {
+//   // Express will serve up production assets
+//   // like our main.js file, or main.css file!
+//   app.use(express.static('build'));
+
+//   // Express will serve up the index.html file
+//   // if it doesn't recognize the route
+//   const path = require('path');
+//   app.get('*', (req, res) => {
+//     res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+//   });
+// }
+
+// const PORT = process.env.PORT || 3000;
+// app.listen(PORT);
