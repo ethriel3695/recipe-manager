fadce5d135e8fdbf463fc70562fdf6c29360a082
Reuben Ellis <ethriel3695@gmail.com>
2018-03-12 15:32:43 -0600

    console.log for res

--- src/actions/authActions.js
+++ src/actions/authActions.js
@@ -3,6 +3,7 @@ import { FETCH_USER } from './actionTypes';
 
 export const fetchUser = () => async dispatch => {
     const res = await axios.get(`${process.env.API_HOST}/api/current_user`);
+    console.log(res);
     console.log(res.data);
     dispatch({ type: FETCH_USER, payload: res.data });
 };
