a689945d5f7d1e8550ab35784da001885ad5d8f4
Reuben Ellis <ethriel3695@gmail.com>
2018-03-12 08:13:55 -0600

    testing heroku deploy

--- src/actions/authActions.js
+++ src/actions/authActions.js
@@ -2,7 +2,7 @@ import axios from 'axios';
 import { FETCH_USER } from './actionTypes';
 
 export const fetchUser = () => async dispatch => {
-    const res = await axios.get(`https://boiling-shore-39277.herokuapp.com/api/current_user`);
+    const res = await axios.get('https://boiling-shore-39277.herokuapp.com/api/current_user');
 
     dispatch({ type: FETCH_USER, payload: res.data });
 };
