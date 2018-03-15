df15650e05af0ffb03df0b8494f2966ebd921b24
Reuben Ellis <ethriel3695@gmail.com>
2018-03-14 22:35:23 -0600

    added axios.default.withcredentials

--- src/actions/authActions.js
+++ src/actions/authActions.js
@@ -4,7 +4,7 @@ import { FETCH_USER } from './actionTypes';
 const API_URL = encodeURI('https://boiling-shore-39277.herokuapp.com');
 
 export const fetchUser = () => async dispatch => {
-    const res = await axios.get(`${API_URL}/api/current_user`);
+    const res = await axios.get(`${API_URL}/api/current_user`, axios.defaults.withCredentials = true);
     console.log(res);
     dispatch({ type: FETCH_USER, payload: res.data });
 };
